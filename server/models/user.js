9da7d24ba17d5b3ef1c1d3e7bf2a48a19b0955ba
brandonb <brandon.barron@concur.com>
2016-04-09 14:32:00 -0700

    updated user schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,11 +2,13 @@ import thinky from '../util/thinky'
 const type = thinky.type
 
 const User = thinky.createModel("User", {
-    email     : type.string().email(),
-    name      : type.string().min(1).max(30).alphanum(),
-    password  : type.string().min(5).max(20),
-    age       : type.number().min(1).max(99),
-    ethnicity : type.string().enum('white', 'black', 'latino', 'asian')
+    email       : type.string().email().required(),
+    name        : type.string().min(1).max(30).alphanum().required(),
+    password    : type.string().min(5).max(20).required(),
+    age         : type.number().min(1).max(99).required(),
+    ethnicity   : type.string().enum('white', 'black', 'latino', 'asian').optional(),
+    favorites   : type.array().optional(),
+    preferences : type.object().required()
 }, {pk : "email"});
 
 export default User;
